690407fa1e9fadba3087e7ed6a387ab1e6d0b494
arturSzuta <artszuta@hotmail.com>
2022-11-26 17:58:31 +0100

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,84 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Integer number = 0;
+        // If - else if - else - condition chain
+        if (number == 0) {
+            System.out.println("Zero");
+        } else if (number == 1) {
+            System.out.println("One");
+        } else if (number == 2) {
+            System.out.println("Two");
+        } else if (number == 3) {
+            System.out.println("Three");
+        } else if (number == 4) {
+            System.out.println("Four");
+        } else if (number == 5) {
+            System.out.println("Five");
+        } else if (number == 6) {
+            System.out.println("Six");
+        } else if (number == 7) {
+            System.out.println("Seven");
+        } else if (number == 8) {
+            System.out.println("Eight");
+        } else if (number == 9) {
+            System.out.println("Nine");
+        } else {
+            System.out.println("I don't know");
+        }
+
+        // Switch statment
+        switch (number) {
+            case 0:
+                System.out.println("Zero");
+                break;
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            default:
+                System.out.println("I don't know");
+                break;
+        }
+
+        // Switch expression
+        String message = switch (number) {
+            case 0 -> "Zero";
+            case 1 -> "One";
+            case 2 -> "Two";
+            case 3 -> "Three";
+            case 4 -> "Four";
+            case 5 -> "Five";
+            case 6 -> "Six";
+            case 7 -> "Seven";
+            case 8 -> "Eight";
+            case 9 -> "Nine";
+            default -> "I don't know";
+        };
+        System.out.println(message);
     }
 }
